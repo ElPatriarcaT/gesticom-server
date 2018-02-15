1b9d03fd88169991c0e7794729ca3070b177bf9e
a023319520g <a023319520g@S09-PC04>
2018-02-15 09:23:43 +0100

    Eventos Ordenados

--- src/main/java/eu/rafaelaznar/dao/specificimplementation/EventosSpecificDaoImplementation.java
+++ src/main/java/eu/rafaelaznar/dao/specificimplementation/EventosSpecificDaoImplementation.java
@@ -5,9 +5,21 @@
  */
 package eu.rafaelaznar.dao.specificimplementation;
 
+import eu.rafaelaznar.bean.genericimplementation.ViewGenericBeanImplementation;
+import eu.rafaelaznar.bean.helper.FilterBeanHelper;
 import eu.rafaelaznar.bean.helper.MetaBeanHelper;
+import eu.rafaelaznar.bean.meta.helper.MetaObjectGenericBeanHelper;
+import eu.rafaelaznar.bean.meta.helper.MetaPropertyGenericBeanHelper;
+import eu.rafaelaznar.bean.publicinterface.GenericBeanInterface;
 import eu.rafaelaznar.dao.genericimplementation.TableGenericDaoImplementation;
+import eu.rafaelaznar.factory.BeanFactory;
+import eu.rafaelaznar.helper.Log4jHelper;
+import eu.rafaelaznar.helper.SqlHelper;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 /**
  *
@@ -17,5 +29,45 @@ public class EventosSpecificDaoImplementation extends TableGenericDaoImplementat
 
     public EventosSpecificDaoImplementation(Connection oPooledConnection, MetaBeanHelper oPuserBean_security, String strWhere) throws Exception {
         super("eventos", oPooledConnection, oPuserBean_security, strWhere);
+        strSQL ="SELECT * FROM eventos WHERE 1 = 1 ORDER BY (fecha)DESC" ;
+    }
+    
+    
+    @Override
+    public MetaBeanHelper getPage(int intRegsPerPag, int intPage, LinkedHashMap<String, String> hmOrder, ArrayList<FilterBeanHelper> alFilter, int expand) throws Exception {
+        String strSQL1 = strSQL;
+        strSQL1 += SqlHelper.buildSqlFilter(alFilter);
+        strSQL1 += SqlHelper.buildSqlOrder(hmOrder);
+        strSQL1 += SqlHelper.buildSqlLimit(this.getCount(alFilter), intRegsPerPag, intPage);
+        ArrayList<ViewGenericBeanImplementation> aloBean = new ArrayList<>();
+        PreparedStatement oPreparedStatement = null;
+        ResultSet oResultSet = null;
+        MetaBeanHelper oMetaBeanHelper = null;
+        try {
+            oPreparedStatement = oConnection.prepareStatement(strSQL1);
+            oResultSet = oPreparedStatement.executeQuery(strSQL1);
+            while (oResultSet.next()) {
+                GenericBeanInterface oBean = BeanFactory.getBean(ob,oPuserSecurity);
+                oBean = (ViewGenericBeanImplementation) oBean.fill(oResultSet, oConnection, oPuserSecurity, expand);
+                aloBean.add((ViewGenericBeanImplementation) oBean);
+            }
+
+            ArrayList<MetaPropertyGenericBeanHelper> alMetaProperties = this.getPropertiesMetaData();
+            MetaObjectGenericBeanHelper oMetaObject = this.getObjectMetaData();
+            oMetaBeanHelper = new MetaBeanHelper(oMetaObject, alMetaProperties, aloBean);
+
+        } catch (Exception ex) {
+            String msg = this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName() + " ob:" + ob;
+            Log4jHelper.errorLog(msg, ex);
+            throw new Exception(msg, ex);
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+            if (oPreparedStatement != null) {
+                oPreparedStatement.close();
+            }
+        }
+        return oMetaBeanHelper;
     }
 }
