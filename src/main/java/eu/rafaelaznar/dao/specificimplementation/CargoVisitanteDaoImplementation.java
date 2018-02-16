480ddc17e13de5d43c834574f98db4f598e037f9
a023319520g <a023319520g@S09-PC04>
2018-02-16 13:30:06 +0100

    Cambios de Prueba

---
+++ src/main/java/eu/rafaelaznar/dao/specificimplementation/CargoVisitanteDaoImplementation.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.rafaelaznar.dao.specificimplementation;
+
+import eu.rafaelaznar.bean.genericimplementation.TableGenericBeanImplementation;
+import eu.rafaelaznar.bean.helper.MetaBeanHelper;
+import eu.rafaelaznar.dao.genericimplementation.TableGenericDaoImplementation;
+import java.sql.Connection;
+
+/**
+ *
+ * @author a023319520G
+ */
+public class CargoVisitanteDaoImplementation extends TableGenericDaoImplementation {
+
+    public CargoVisitanteDaoImplementation(Connection oPooledConnection, MetaBeanHelper oPuserBean_security, String strWhere) throws Exception {
+        super("cargo", oPooledConnection, oPuserBean_security, strWhere);
+    }
+    
+    @Override
+    public Integer set(TableGenericBeanImplementation oBean) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int remove(Integer id) throws Exception {
+        return 0;
+    }
+}
